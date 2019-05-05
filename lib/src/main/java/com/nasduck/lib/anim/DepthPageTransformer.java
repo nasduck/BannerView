313cf9c409a495de8618c29700b0d24afec5f042
sohnyi <sohnyi@163.com>
2019-05-05 10:05:32 +0800

    添加 5 个常用的 Viewpager 转场动画。

---
+++ lib/src/main/java/com/nasduck/lib/anim/DepthPageTransformer.java
@@ -0,0 +1,35 @@
+package com.nasduck.lib.anim;
+
+import android.support.annotation.NonNull;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
+/**
+ * 效果：
+ * 左滑时当前页面左滑消失，下一个页面渐变上浮
+ * 右滑时当前页面渐变下沉，下一个页面从左边划入
+ */
+
+public class DepthPageTransformer implements ViewPager.PageTransformer {
+    private static final float MIN_SCALE = 0.75f;
+    @Override
+    public void transformPage(@NonNull View view, float position) {
+        int pageWidth = view.getWidth();
+        if (position < -1) {
+            view.setAlpha(0);
+        } else if (position <= 0) {
+            view.setAlpha(1);
+            view.setTranslationX(0);
+            view.setScaleX(1);
+            view.setScaleY(1);
+        } else if (position <= 1) {
+            view.setAlpha(1 - position);
+            view.setTranslationX(pageWidth * - position);
+            float scaleFactor = MIN_SCALE + (1 - MIN_SCALE) * (1 - Math.abs(position));
+            view.setScaleX(scaleFactor);
+            view.setScaleY(scaleFactor);
+        } else {
+            view.setAlpha(0);
+        }
+    }
+}
---
+++ lib/src/main/java/com/nasduck/lib/anim/ZoomOutPageTransformer.java
@@ -0,0 +1,41 @@
+package com.nasduck.lib.anim;
+
+import android.support.annotation.NonNull;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * 效果：
+ * 左右正常切换，有渐隐缩小到全显示放大的过程
+ */
+
+public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+
+    private static final float MIN_SCALE = 0.85f;
+    private static final float MIN_ALPHA = 0.5f;
+
+    @Override
+    public void transformPage(@NonNull View view, float position) {
+        int pageWidth = view.getWidth();
+        int pageHeight = view.getHeight();
+        Log.e("TAG", view + " , " + position + "");
+        if (position < -1) {
+            view.setAlpha(0);
+        } else if (position <= 1) {
+            float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+            float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+            float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+            if (position < 0) {
+                view.setTranslationX(horzMargin - vertMargin / 2);
+            } else {
+                view.setTranslationX(-horzMargin + vertMargin / 2);
+            }
+            view.setScaleX(scaleFactor);
+            view.setScaleY(scaleFactor);
+            view.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE) / (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+        } else {
+            view.setAlpha(0);
+        }
+    }
+}
