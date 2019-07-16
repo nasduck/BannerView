c4f68999ea7db9bfb835a8458b55f1eec05234c0
Xiaoliang Yang <xiaoliang.yang@nasduck.cn>
2019-07-16 14:22:28 +0800

    添加 ImageLoader

---
+++ lib/src/main/java/com/nasduck/lib/loader/ImageLoader.java
@@ -0,0 +1,13 @@
+package com.nasduck.lib.loader;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+public abstract class ImageLoader implements ImageLoaderInterface<ImageView> {
+
+    @Override
+    public ImageView createImageView(Context context) {
+        ImageView imageView = new ImageView(context);
+        return imageView;
+    }
+}
